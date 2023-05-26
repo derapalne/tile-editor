ef0ac688f1b3f8502abd25a1262ccd898b541dab
derapalne <derapalne@gmail.com>
2023-05-26 14:09:29 -0300

    Drag and paint + clouds

--- layout.js
+++ layout.js
@@ -3,8 +3,9 @@ function getById(id) {
 }
 
 let editable = true;
+let clicking = false;
 let currentSprite = "01";
-const totalSprites = 10;
+const totalSprites = 11;
 
 const cellSeparator = ",";
 const rowSeparatosr = ";";
@@ -20,19 +21,46 @@ screenContainer.appendChild(currentSpriteElement);
 const screenContent = document.createElement("div");
 screenContent.classList.add("screen-content");
 
+function spriteClickHandler(e, cellCoords) {
+    if (e.shiftKey) {
+        return fillAreaWithSprite(cellCoords);
+    }
+    paintSprite(cellCoords);
+}
+
+function spriteRightClickHandler(e, cellCoords) {
+    e.preventDefault();
+    if (e.shiftKey) {
+        return pickSprite(cellCoords);
+    }
+    eraseSprite(cellCoords);
+}
+
+function spriteMouseDownHandler(e) {
+    e.preventDefault();
+    clicking = true;
+}
+
+function spriteMouseUpHandler(e) {
+    clicking = false;
+}
+
+function spriteMouseOverHandler(e, cellCoords) {
+    if(clicking) {
+        paintSprite(cellCoords);
+    }
+}
+
 function toggleEditMode() {
     if (editable) {
         const background = document.querySelectorAll(".cell");
-        background.forEach((item) => {
-            item.classList.remove("editable");
-            item.removeEventListener("contextmenu", (e) => {
-                e.preventDefault();
-                if (e.shiftKey) {
-                    return pickSprite(cellCoords);
-                }
-                eraseSprite(cellCoords);
-            });
-            item.removeAttribute("onclick");
+        background.forEach((cell) => {
+            cell.classList.remove("editable");
+            cell.removeEventListener("contextmenu", (e) => spriteRightClickHandler(e, cellCoords));
+            cell.removeEventListener("click", (e) => spriteClickHandler(e, cellCoords));
+            cell.removeEventListener("mousedown", (e) => spriteMouseDownHandler(e));
+            cell.removeEventListener("mouseup", (e) => spriteMouseUpHandler(e));
+            cell.removeEventListener("mouseover", (e) => spriteMouseOverHandler(e));
         });
         editButton.innerText = "Editable: ❌";
     } else {
@@ -40,15 +68,12 @@ function toggleEditMode() {
             for (let x = 0; x < 32; x++) {
                 const cellCoords = `${x}-${23 - y}`;
                 const cell = getById(`cell-${cellCoords}`);
-                cell.addEventListener("contextmenu", (e) => {
-                    e.preventDefault();
-                    if (e.shiftKey) {
-                        return pickSprite(cellCoords);
-                    }
-                    eraseSprite(cellCoords);
-                });
-                cell.classList.add("cell", "editable");
-                cell.setAttribute("onclick", `paintSprite('${cellCoords}')`);
+                cell.classList.add("editable");
+                cell.addEventListener("contextmenu", (e) => spriteRightClickHandler(e, cellCoords));
+                cell.addEventListener("click", (e) => spriteClickHandler(e, cellCoords));
+                cell.addEventListener("mousedown", (e) => spriteMouseDownHandler(e));
+                cell.addEventListener("mouseup", (e) => spriteMouseUpHandler(e));
+                cell.addEventListener("mouseover", (e) => spriteMouseOverHandler(e));
             }
         }
         editButton.innerText = "Editable: ✔";
@@ -91,6 +116,38 @@ function pickSprite(coords) {
     currentSpriteElement.src = `img/${currentSprite}.png`;
 }
 
+function fillAreaWithSprite(coords) {
+    const rootX = coords.split("-")[0];
+    const rootY = coords.split("-")[1];
+    const cell = getById(`cell-${coords}`);
+    const sprite = cell.children.item(0);
+    sprite.src = `img/${currentSprite}.png`;
+    const matchingTile = sprite.dataset.sprite;
+    console.log(matchingTile, rootX, rootY);
+    let differentPY = false;
+    let cursorPy = 0;
+    while (!differentPY) {
+        if (Number(rootY) + cursorPy > 23) break;
+        let cursorPx = 1;
+        let differentPX = false;
+        while (!differentPX) {
+            const newCellCoords = `${Number(rootX) + cursorPx}-${Number(rootY) + cursorPy}`;
+            if (Number(rootX) + cursorPx > 31) break;
+            console.log(newCellCoords);
+            const newCell = getById(`cell-${newCellCoords}`);
+            const newSprite = newCell.children.item(0);
+            if (newSprite.dataset.sprite === matchingTile) {
+                newSprite.src = `img/${currentSprite}.png`;
+            } else {
+                if (cursorPx === 1) differentPY = true;
+                differentPX = true;
+            }
+            cursorPx++;
+        }
+        cursorPy++;
+    }
+}
+
 function initScreenEditable(preset) {
     let filling, topping;
     switch (preset) {
@@ -121,22 +178,19 @@ function initScreenEditable(preset) {
             const cellCoords = `${x}-${23 - y}`;
             cell.setAttribute("id", `cell-${cellCoords}`);
             cell.classList.add("cell", "editable");
-            cell.setAttribute("onclick", `paintSprite('${cellCoords}')`);
-            cell.addEventListener("contextmenu", (e) => {
-                e.preventDefault();
-                if (e.shiftKey) {
-                    return pickSprite(cellCoords);
-                }
-                eraseSprite(cellCoords);
-            });
+            cell.addEventListener("contextmenu", (e) => spriteRightClickHandler(e, cellCoords));
+            cell.addEventListener("click", (e) => spriteClickHandler(e, cellCoords));
+            cell.addEventListener("mousedown", (e) => spriteMouseDownHandler(e));
+            cell.addEventListener("mouseup", (e) => spriteMouseUpHandler(e));
+            cell.addEventListener("mouseover", (e) => spriteMouseOverHandler(e, cellCoords));
             const sprite = document.createElement("img");
             if (y > 18) {
                 sprite.src = filling;
-                sprite.setAttribute("data-sprite", filling.substring(4,6));
+                sprite.setAttribute("data-sprite", filling.substring(4, 6));
             } else {
                 if (y === 18) {
                     sprite.src = topping;
-                    sprite.setAttribute("data-sprite", topping.substring(4,6));
+                    sprite.setAttribute("data-sprite", topping.substring(4, 6));
                 } else {
                     sprite.src = "img/00.png";
                     sprite.setAttribute("data-sprite", "00");
